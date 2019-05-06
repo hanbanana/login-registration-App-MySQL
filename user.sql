7071b4eed37368fa0cf91489073583c6b1fbe895
Hansung Lee <hanlee@GOLD-POINT.COM>
2019-05-06 09:07:18 -0700

    add file

---
+++ user.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS mydb;
+CREATE database mydb;
+
+USE mydb;
+
+CREATE TABLE `users`(
+`id` int(11) NOT NULL AUTO_INCREMENT, 
+`name` varchar(255) NOT NULL, 
+`email` varchar(255) NOT NULL, 
+`password` varchar(255) NOT NULL, 
+`created_at` datetime NOT NULL, 
+`updated_at` datetime NOT NULL, 
+PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1
